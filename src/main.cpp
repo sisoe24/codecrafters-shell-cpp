8f88da0c6004ae2a157877da83a8fcf586c9760d
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-22 15:09:20 -0500

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+    while (true) {
+        std::string input;
+        std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << '\n';
+        std::cout << input << ": command not found" << '\n';
+        std::cout << "$ ";
+    }
 }
