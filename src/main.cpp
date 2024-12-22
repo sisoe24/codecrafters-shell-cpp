d11b84e3847f7e9d0c8d9c1d8dec4958e0853966
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-22 15:11:04 -0500

    The exit builtin

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ int main() {
         std::string input;
         std::getline(std::cin, input);
 
+        if (input == "exit 0") {
+            return 0;
+        }
+
         std::cout << input << ": command not found" << '\n';
         std::cout << "$ ";
     }
