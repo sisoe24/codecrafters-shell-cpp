d3f2869d2c6c9ae526ae1f8402c1b936a9ca862e
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-22 15:04:38 -0500

    Print a prompt

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
+  std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
